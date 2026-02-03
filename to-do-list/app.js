2daf09702e5dde452d8f2f7c5bae7325c45ebc01
smritisethi95 <smriti.sethi95@gmail.com>
2026-02-03 09:38:53 +0530

    checkbutton, edit button added, improved UI

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -3,20 +3,34 @@ let item = document.getElementById('add-item');
 let ul = document.getElementById('item-list');
 
 
-addBtn.addEventListener('click', function(e){
-  let li = document.createElement('li');
-  li.id = 'item';
-  li.textContent=item.value;
-  let delBtn = document.createElement('button');
-  delBtn.id = 'btn-del';
-  delBtn.className = 'delete-btn';
-  delBtn.textContent = 'X';
-  li.append(delBtn);
+addBtn.addEventListener('click', function (e) {
+  let li = createButtonsEditAndDelete('li', 'item', '', '');
+  let checkBtn = createButtonsEditAndDelete('input', 'check-btn', 'check-btn', item.value, 'checkbox', item.value);
+  let btnContainer = createButtonsEditAndDelete('div', 'btn-wrapper', 'btn-wrapper', '');
+  let delBtn = createButtonsEditAndDelete('button', 'btn-del', 'delete-btn', 'X');
+  let editBtn = createButtonsEditAndDelete('button', 'btn-edit', 'edit-btn', 'Edit');
+  btnContainer.append(editBtn, delBtn);
+  li.append(checkBtn, btnContainer);
   ul.append(li);
 });
 
-ul.addEventListener('click', function(event){
-  if(event.target.classList.contains('delete-btn')){
-    event.target.parentElement.remove();
+function createButtonsEditAndDelete(type, id, cssClass, btnLabel, inputType, inputValue) {
+  let element = document.createElement(type);
+  element.id = id;
+  element.className = cssClass;
+  element.textContent = btnLabel;
+  if (type === 'input') {
+    element.type = inputType || 'text';
+    if (inputValue !== undefined) {
+      element.value = inputValue;
+    }
+  }
+
+  return element;
+}
+
+ul.addEventListener('click', function (event) {
+  if (event.target.classList.contains('delete-btn')) {
+    event.target.parentElement.parentElement.remove();
   }
 })
